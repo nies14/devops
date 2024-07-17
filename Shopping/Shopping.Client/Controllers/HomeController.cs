fcf7e05c580a782486703e2c849531d04184f869
nies14 <mthnies@gmail.com>
2024-07-17 17:06:33 -0400

    initial commit

---
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Client.Data;
+
+namespace Shopping.Client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View(ProductContext.Products);
+        }
+    }
+}
